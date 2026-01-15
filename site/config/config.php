6deb0d62821614a7f01aa4f7dc0f4d4a8bc32ef2
nondidjos <nathan@noirextreme.com>
2026-01-15 16:58:00 +0100

    perms: added author role. can only edit 'notes'

--- site/config/config.php
+++ site/config/config.php
@@ -10,4 +10,13 @@
 return [
     'debug' => true,
     'yaml.handler' => 'symfony', // already makes use of the more modern Symfony YAML parser: https://getkirby.com/docs/reference/system/options/yaml (will become the default in a future Kirby version)
+    'hooks' => [
+        'page.changeStatus:before' => function ($page, $status, $oldStatus) {
+            $user = $this->user();
+            // Block 'author' role from publishing (changing state to 'listed')
+            if ($user && $user->role()->name() === 'author' && $status === 'listed') {
+                throw new Exception('You are not allowed to publish pages. Please submit for review instead.');
+            }
+        }
+    ]
 ];
